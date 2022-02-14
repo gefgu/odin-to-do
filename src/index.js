0a899e1df1f44cdb5659e503bd8ac4ddc469189c
gefgu <gefgu@hotmail.com>
2022-02-14 19:56:10 -0300

    Add main modules info

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 // Information Holders: Store info and provide it. Also has functions to change it.
 // Todo factory function.
 // Projects factory function.
+
+// appController - controls the application logic.
+// appUIProvider - controls the UI.
+// appInterfacer - controls calls between appController and appUIProvider.
