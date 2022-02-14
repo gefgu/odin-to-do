d92c8d33281484f23aed570dd07044f34654b72b
gefgu <gefgu@hotmail.com>
2022-02-14 19:41:24 -0300

    Add htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo App",
+    }),
+  ],
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
   },
 };
