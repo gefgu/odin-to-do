0f7ec7e262a822876506509b4b9ad68515483e2d
gefgu <gefgu@hotmail.com>
2022-02-17 19:37:32 -0300

    Add get and add functions to app controller

---
+++ src/app-controller.js
@@ -0,0 +1,15 @@
+import todoFactory from "./todo-factory.js";
+
+export default (() => {
+  let tasks = [];
+
+  const getToDos = () => {
+    return tasks;
+  };
+
+  const addToDo = (title, description, dueDate, priority) => {
+    tasks.splice(0, 0, todoFactory(title, description, dueDate, priority));
+  };
+
+  return { getToDos, addToDo };
+})();
