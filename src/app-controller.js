079e2981a18941ccd15e027da15fccb79c552be7
gefgu <gefgu@hotmail.com>
2022-02-20 19:27:37 -0300

    Add new project dummy data

--- src/app-controller.js
+++ src/app-controller.js
@@ -2,19 +2,21 @@ import todoFactory from "./todo-factory.js";
 import projectFactory from "./project-factory.js";
 
 export default (() => {
-  let projects = { inbox: projectFactory("inbox") };
+  let projects = {
+    inbox: projectFactory("inbox"),
+    home: projectFactory("home"),
+  };
   projects.inbox.addToDo(todoFactory("First Todo", "Info", "Tomorrow", "A"));
+  projects.home.addToDo(todoFactory("Home To-Do", "Info", "Next Week", "A"));
 
   const getToDos = (projectName) => {
     return projects[projectName].getToDos();
   };
 
   const addToDo = (projectName, title, description, dueDate, priority) => {
-    projects[projectName].getToDos().splice(
-      0,
-      0,
-      todoFactory(title, description, dueDate, priority)
-    );
+    projects[projectName]
+      .getToDos()
+      .splice(0, 0, todoFactory(title, description, dueDate, priority));
   };
 
   const removeToDo = (projectName, index) => {
